ca70d01e54cd30697faabd5fb66b47fe4c8e022e
Alan Renouf <ajw.renouf@gmail.com>
2016-07-26 22:35:41 -0700

    Update pester and add vrops script
    Updated Pester test so it can be called with parameters and included
    vROPS Maintenance Mode Script

--- Pester/Test Connection to VC.ps1
+++ Pester/Test Connection to VC.ps1
@@ -4,11 +4,15 @@ Created on: 07/15/2016
 Author: Alan Renouf, @alanrenouf
 Description: The purpose of this pester test is to ensure the PowerCLI modules are imported and a connection and disconnection can be made to a vCenter
 Dependencies: Pester Module
+Example run:
+
+Invoke-Pester -Script @{ Path = '.\Test Connection to VC.ps1'; Parameters = @{ VCNAME="VC01.local"; VCUSER="Administrator@vsphere.local"; VCPASS="Admin!23"} }
+
 #>
 
-$VCNAME = "MyVC@Mydomain.local"
-$VCUSER = "Administrator@vsphere.local"
-$VCPASS = "Admin!23"
+$VCUSER = $Parameters.Get_Item("VCUSER")
+$VCPASS = $Parameters.Get_Item("VCPASS")
+$VCNAME = $Parameters.Get_Item("VCNAME")
 
 Describe "PowerCLI Tests" {
     It "Importing PowerCLI Modules" {
