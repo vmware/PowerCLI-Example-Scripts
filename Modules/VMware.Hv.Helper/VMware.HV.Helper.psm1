4b106b9cfd06db74d6722bed7af6ab125609a121
rpstester <32249806+rpstester@users.noreply.github.com>
2017-10-25 14:08:58 -0400

    New Year
    Update Copyright year

--- Modules/VMware.Hv.Helper/VMware.HV.Helper.psm1
+++ Modules/VMware.Hv.Helper/VMware.HV.Helper.psm1
@@ -1,7 +1,7 @@
 #Script Module : VMware.Hv.Helper
 #Version       : 1.2
 
-#Copyright © 2016 VMware, Inc. All Rights Reserved.
+#Copyright © 2017 VMware, Inc. All Rights Reserved.
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of
 #this software and associated documentation files (the "Software"), to deal in
