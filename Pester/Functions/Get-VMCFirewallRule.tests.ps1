bbc6d0f77550bdf688c25746df10db038b4f541c
Conor Tolan <ctolan@gmail.com>
2019-02-27 22:23:05 +0000

    Started tests for VMCFirewall. Fixed incorrect function call in sddc version test.

--- Pester/Functions/Get-VMCFirewallRule.tests.ps1
+++ Pester/Functions/Get-VMCFirewallRule.tests.ps1
@@ -2,15 +2,17 @@
 Import-Module -Name VMware.VimAutomation.Cis.Core
 
 inModuleScope VMware.VMC {
-    $functionName = "Get-VMCSDDCVersion"
+    $functionName = "Get-VMCFirewallRule"
     Describe "$functionName" -Tag 'Unit' {
         . "$PSScriptRoot/Shared.ps1"
 
         $global:DefaultVMCServers = $true
 
-        $Service = "com.vmware.vmc.orgs.sddcs"
+        $Service = "com.vmware.vmc.orgs.sddcs.networks.edges.firewall.config"
         $OrgId = "Mocked OrgID"
+        $GatewayType = "MGW"
         $SddcName = "MockedSDDCName"
+        $OrgName = "MockedOrgName"
         $version = "MockedVersion"
 
         $orgs = @(
@@ -30,9 +32,11 @@ inModuleScope VMware.VMC {
         })
 
         Mock -CommandName Get-VMCOrg -MockWith { $orgs }
+        Mock -CommandName Get-VMCSDDC -MockWith { $orgs }
 
         Mock -CommandName Get-VMCService -MockWith { $MockedServiceObj }
 
+        $MockedServiceObj | Add-Member -MemberType ScriptMethod -Name "Get" -Value { $ServicesObject }
         $MockedServiceObj | Add-Member -MemberType ScriptMethod -Name "list" -Value { $ServicesObject }
 
         Mock -CommandName Write-Error -MockWith {}
@@ -40,34 +44,30 @@ inModuleScope VMware.VMC {
         Context "Sanity checking" {
             $command = Get-Command -Name $functionName
 
-            defParam $command 'Org'
-            defParam $command 'Name'
+            defParam $command 'OrgName'
+            defParam $command 'SDDCName'
+            defParam $command 'ShowAll'
+            defParam $command 'GatewayType'
         }
 
         Context "Behavior testing" {
             # Testing single Org with optional SDDC parameter
-            It "calls Get-VMCSDDCVersion with the Org name supplied" {
-                { Get-VMCSDDCVersion -Org $OrgId -Name $SddcName} | Should Not Throw
-                Assert-MockCalled -CommandName Get-VMCOrg -Times 1 -Scope It -ParameterFilter { $Name -eq $OrgId }
+            It "calls Get-VMCFirewallRule with the Org name supplied" {
+                { Get-VMCFirewallRule -GatewayType $GatewayType -SDDCName $SddcName -OrgName $OrgName} | Should Not Throw
+                Assert-MockCalled -CommandName Get-VMCOrg -Times 1 -Scope It #-ParameterFilter { $Name -eq $SddcName }
+                Assert-MockCalled -CommandName Get-VMCSDDC -Times 1 -Scope It -ParameterFilter { $Name -eq $SddcName -and $Org -eq $OrgName }
             }
             It "calls get-service to com.vmware.vmc.orgs.sddcs" {
-                { Get-VMCSDDCVersion -Org $OrgId } | Should Not Throw
+                { Get-VMCFirewallRule -GatewayType $GatewayType } | Should Not Throw
                 Assert-MockCalled -CommandName Get-VMCService -Times 1 -Scope It -ParameterFilter { $name -eq $Service }
             }
             # Testing a single SDDC response
             It "gets the task details via list method and returns the properties" {
-                $(Get-VMCSDDCVersion -Org $OrgId).version  | Should -be $version
-            }
-            # Testing the multiple SDDC response
-            It "gets the task details of the Org supplied and returns the properties" {
-                Mock -CommandName Get-VMCOrg -MockWith { @($orgs, $orgs) }
-
-                $(Get-VMCSDDCVersion -Org $OrgId)[0].version  | Should -be $version
-                $(Get-VMCSDDCVersion -Org $OrgId)[1].version  | Should -be $version
+                $(Get-VMCFirewallRule -GatewayType $GatewayType).version  | Should -be $version
             }
             It "writes an error if not connected" {
                 $global:DefaultVMCServers = $false
-                { Get-VMCSDDCVersion -Org $OrgId  } | Should Not Throw
+                { Get-VMCFirewallRule -GatewayType $GatewayType } | Should Not Throw
                 Assert-MockCalled -CommandName Write-Error -Times 1 -Scope It -ParameterFilter { $org -eq $Org -and $Sddc -eq $Sddc  }
             }
         }
