f3c549a8671644e1fa513076dba998ba994f0007
William Lam <william2003@gmail.com>
2020-02-28 10:34:01 -0800

    Consolidated VCSA functions into VCSA Module

--- Modules/VCSA/VCSA.psm1
+++ Modules/VCSA/VCSA.psm1
@@ -37,3 +37,32 @@ Function Get-VCSAPasswordPolicy {
         Write-Host "`nUnable to find VCSA named $VCSAName"
     }
 }
+
+Function Get-VCSAIdentitySource {
+<#
+    .DESCRIPTION Retrieves vCenter Server Appliance Identity Source Configuration
+    .NOTES  Author:  William Lam
+    .PARAMETER VCSAName
+        Inventory name of the VCSA VM
+    .PARAMETER VCSARootPassword
+        Root password for VCSA VM
+    .EXAMPLE
+        Get-VCSAIdentitySource -VCSAName "MGMT-VCSA-01" -VCSARootPassword "VMware1!"
+#>
+    Param (
+        [Parameter(Mandatory=$true)][String]$VCSAName,
+        [Parameter(Mandatory=$true)][String]$VCSARootPassword
+    )
+
+    $vm = Get-Vm -Name $VCSAName
+
+    if($vm) {
+        $identitySources = Invoke-VMScript -ScriptText "/opt/vmware/bin/sso-config.sh -get_identity_sources 2> /dev/null | sed -ne '/^*/,$ p'" -vm $vm -GuestUser "root" -GuestPassword $VCSARootPassword
+
+        Write-Host -ForegroundColor green "`nIdentity Sources: "
+        $identitySources
+
+    } else {
+        Write-Host "`nUnable to find VCSA named $VCSAName"
+    }
+}
