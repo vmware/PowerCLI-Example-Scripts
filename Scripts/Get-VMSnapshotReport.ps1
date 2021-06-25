04ba5e75251916b8766c65dfdd4d27dc5f574df2
Jimit G <jimitkg@gmail.com>
2021-06-25 22:31:08 +1000

    fomating corrections using prettier extension

--- Scripts/Get-VMSnapshotReport.ps1
+++ Scripts/Get-VMSnapshotReport.ps1
@@ -34,119 +34,135 @@
  #>
 function Get-VMSnapshotReport {
 
-[CmdletBinding(DefaultParametersetName="Disabled")]
-Param(
-  [Parameter(Mandatory=$True,Position=1,ValueFromPipeline=$True)]
-  [ValidateNotNullOrEmpty()]
-  [string]$VIServerFilePath,
+    [CmdletBinding(DefaultParametersetName = "Disabled")]
+    Param(
+        [Parameter(Mandatory = $True, Position = 1, ValueFromPipeline = $True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$VIServerFilePath,
 	
-  [Parameter(Mandatory=$True,Position=2)]
-  [string]$OutPath
-)
-# path to CSV list of viservers
+        [Parameter(Mandatory = $True, Position = 2)]
+        [string]$OutPath
+    )
+    # path to CSV list of viservers
 
-$viservers = (Get-Content "VIServerFilePath")
+    $viservers = (Get-Content "VIServerFilePath")
 
-Connect-VIServer $viservers
+    Connect-VIServer $viservers
 
-$CSS2 =  "body {
-    color:#333333;
-    font-family:Tahoma;
-    font-size: 10pt;
-    }
-    h1 {
-    text-align:center;
-    }
-    h2 {
-    border-top:1px solid #666666;
-    }
-    th {
-    font-weight:bold;
-    color:#eeeeee;
-    background-color:#333333;
-    cursor:pointer;
-    }
-    .odd { background-color:#ffffff; }
-    .even { background-color:#bfbfbf; }
-    
-    .paginate_enabled_next, .paginate_enabled_previous {
-    cursor:pointer;
-    border:1px solid #222222;
-    background-color:#dddddd;
-    padding:2px;
-    margin:4px;
-    border-radius:2px;
-    }
-    .paginate_disabled_previous, .paginate_disabled_next {
-    color:#666666;
-    cursor:pointer;
-    background-color:#dddddd;
-    padding:2px;
-    margin:4px;
-    border-radius:2px;
-    }
-    .dataTables_info { margin-bottom:4px; }
-    .sectionheader { cursor:pointer; }
-    .sectionheader:hover { color:red; }
-    .grid { width:100% }
-    .smallgrid { width:50% }
-    .red {
-    color:#ff3333;
-    font-weight:bold;
-    }
-    .green {
-    background-color:#00cc44;
-    font-weight:bold;
-    }
+    $CSS2 = "body {
+        color: #333333;
+        font-family: Tahoma;
+        font-size: 10pt;
+      }
+      h1 {
+        text-align: center;
+      }
+      h2 {
+        border-top: 1px solid #666666;
+      }
+      th {
+        font-weight: bold;
+        color: #eeeeee;
+        background-color: #333333;
+        cursor: pointer;
+      }
+      .odd {
+        background-color: #ffffff;
+      }
+      .even {
+        background-color: #bfbfbf;
+      }
+      
+      .paginate_enabled_next,
+      .paginate_enabled_previous {
+        cursor: pointer;
+        border: 1px solid #222222;
+        background-color: #dddddd;
+        padding: 2px;
+        margin: 4px;
+        border-radius: 2px;
+      }
+      .paginate_disabled_previous,
+      .paginate_disabled_next {
+        color: #666666;
+        cursor: pointer;
+        background-color: #dddddd;
+        padding: 2px;
+        margin: 4px;
+        border-radius: 2px;
+      }
+      .dataTables_info {
+        margin-bottom: 4px;
+      }
+      .sectionheader {
+        cursor: pointer;
+      }
+      .sectionheader:hover {
+        color: red;
+      }
+      .grid {
+        width: 100%;
+      }
+      .smallgrid {
+        width: 50%;
+      }
+      .red {
+        color: #ff3333;
+        font-weight: bold;
+      }
+      .green {
+        background-color: #00cc44;
+        font-weight: bold;
+      }      
     "
 
-#Capturing VMs data
-$vms = Get-VM
+    #Capturing VMs data
+    $vms = Get-VM
 
-#reportgeneratedtag
-$reportgeneratedtag = (Get-Date -Format "dd MMMM yyyy HH:mm:ss")
+    #reportgeneratedtag
+    $reportgeneratedtag = (Get-Date -Format "dd MMMM yyyy HH:mm:ss")
 
-#Capturing data for snapshots
+    #Capturing data for snapshots
 
-    $paramsSnapshotFragment = @{'As'='Table';
-            'PreContent'='<h2>VM Snapshots</h2>';
-            'MakeTableDynamic'=$true;
-            'TableCssClass'='grid';
-            'Properties' = 'VM',@{Name = 'SnapshotName';Expression = {$_.Name}},'Created', @{Name = 'AgeDays';Expression = {((New-TimeSpan -Start $_.Created -End (get-date)).Days)};css={if (((New-TimeSpan -Start $_.Created -End (get-date)).Days) -gt 120) {'red'}}},'Description', @{name = 'SizeGB';expression = {[math]::Round($_.sizegb,2)};css={if ($_.sizegb -gt 100) {'red'}}}, 'PowerState','IsCurrent','ParentSnapshot','Children'
+    $paramsSnapshotFragment = @{'As' = 'Table';
+        'PreContent'                 = '<h2>VM Snapshots</h2>';
+        'MakeTableDynamic'           = $true;
+        'TableCssClass'              = 'grid';
+        'Properties'                 = 'VM', @{Name = 'SnapshotName'; Expression = { $_.Name } }, 'Created', @{Name = 'AgeDays'; Expression = { ((New-TimeSpan -Start $_.Created -End (get-date)).Days) }; css = { if (((New-TimeSpan -Start $_.Created -End (get-date)).Days) -gt 120) { 'red' } } }, 'Description', @{name = 'SizeGB'; expression = { [math]::Round($_.sizegb, 2) }; css = { if ($_.sizegb -gt 100) { 'red' } } }, 'PowerState', 'IsCurrent', 'ParentSnapshot', 'Children'
     }
     $snapshots = $vms | Get-Snapshot | ConvertTo-EnhancedHTMLFragment @paramsSnapshotFragment 
 
-#PoweredOff
+    #PoweredOff
 
-$paramsPoweredoff = @{'As'='Table';
-'PreContent'='<h2>Powered Off VMs</h2>';
-'MakeTableDynamic'=$true;
-'TableCssClass'='grid';
-'Properties' = 'Name','PowerState', 'Notes' , 'Folder'
-}
-$poweredoffvms = $vms | Where-Object {$_.PowerState -ne 'PoweredOn'} | ConvertTo-EnhancedHTMLFragment @paramsPoweredoff   
+    $paramsPoweredoff = @{'As' = 'Table';
+        'PreContent'           = '<h2>Powered Off VMs</h2>';
+        'MakeTableDynamic'     = $true;
+        'TableCssClass'        = 'grid';
+        'Properties'           = 'Name', 'PowerState', 'Notes' , 'Folder'
+    }
+    $poweredoffvms = $vms | Where-Object { $_.PowerState -ne 'PoweredOn' } | ConvertTo-EnhancedHTMLFragment @paramsPoweredoff   
 
-#PoweredOn
+    #PoweredOn
 
-$paramsPoweredon = @{'As'='Table';
-'PreContent'='<h2>Powered On VMs</h2>';
-'MakeTableDynamic'=$true;
-'TableCssClass'='grid';
-'Properties' = 'Name','PowerState', 'Notes' , 'Folder'
-}
-$poweredonvms = $vms | Where-Object {$_.PowerState -eq 'PoweredOn'} | ConvertTo-EnhancedHTMLFragment @paramsPoweredon   
+    $paramsPoweredon = @{'As' = 'Table';
+        'PreContent'          = '<h2>Powered On VMs</h2>';
+        'MakeTableDynamic'    = $true;
+        'TableCssClass'       = 'grid';
+        'Properties'          = 'Name', 'PowerState', 'Notes' , 'Folder'
+    }
+    $poweredonvms = $vms | Where-Object { $_.PowerState -eq 'PoweredOn' } | ConvertTo-EnhancedHTMLFragment @paramsPoweredon   
 
 
     # Finalising main html report
     $paramsMainHTML = @{
-        'HTMLFragments' =  $reportgeneratedtag, $snapshots, $poweredoffvms, $poweredonvms
-        'CssStyleSheet' =  $CSS2;
-        'Title' = 'VM Status Report';
-        'PreContent' = "<h1>VM Status Report</h1>"
+        'HTMLFragments' = $reportgeneratedtag, $snapshots, $poweredoffvms, $poweredonvms
+        'CssStyleSheet' = $CSS2;
+        'Title'         = 'VM Status Report';
+        'PreContent'    = "<h1>VM Status Report</h1>"
     }
     ConvertTo-EnhancedHTML  @paramsMainHTML | Out-File "$OutPath\VMStatusReport.html" -Encoding utf8
 
 
-Disconnect-VIServer -Server $viservers -Force -Confirm:$false
+    Disconnect-VIServer -Server $viservers -Force -Confirm:$false
 }
 
