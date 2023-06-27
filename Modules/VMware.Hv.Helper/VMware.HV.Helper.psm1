2be8d0570775d17c7979b91e63890f0ed1f47440
desrosim232 <131466573+desrosim232@users.noreply.github.com>
2023-06-27 08:08:53 -0400

    Update VMware.HV.Helper.psm1
    Encoding correction
    
    Signed-off-by: Mathieu Desrosiers desrosim@gmail.com

--- Modules/VMware.Hv.Helper/VMware.HV.Helper.psm1
+++ Modules/VMware.Hv.Helper/VMware.HV.Helper.psm1
@@ -1,7 +1,7 @@
 #Script Module : VMware.Hv.Helper
 #Version       : 1.3.1
 
-#Copyright Â© 2016 VMware, Inc. All Rights Reserved.
+#Copyright © 2016 VMware, Inc. All Rights Reserved.
 
 #Permission is hereby granted, free of charge, to any person obtaining a copy of
 #this software and associated documentation files (the "Software"), to deal in
